fc22ac1dbabcd87f8abc58a559c0dccb3f4fee3e
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-09-22 17:26:04 +0900

    [Refactor] for문으로 검사에서 앞뒤 검사하는 방식으로 변경 후 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     private final static int MAX_LENGTH = 1000;
     private final static int MIN_LENGTH = 1;
     private final static String CHECK_LOWER_CASE = "[a-z]+";
-
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
+        while (true){
+            int idx = isDuplicateLetter(answer);
+            if(idx==-1){
+                break;
+            }
+            answer = deleteDuplicateLetter(answer,idx);
+        }
         return answer;
     }
 
     private final static boolean validateInput(String checkStr){
-        if(checkStr.length()<1 || checkStr.length()>1000){
+        if(checkStr.length()<MIN_LENGTH || checkStr.length()>MAX_LENGTH){
             return false;
         }
         return true;
@@ -22,5 +31,25 @@ public class Problem2 {
             return true;
         }
         return false;
+    }
+
+    private final static int isDuplicateLetter(String checkStr){
+
+    }
+    private final static char frontIdx(int idx,String checkStr){
+        if(idx==0){
+            return ' ';
+        }
+        return checkStr.charAt(idx-1);
+    }
+    private final static char backIdx(int idx,String checkStr){
+        if(idx==checkStr.length()-1){
+            return ' ';
+        }
+        return checkStr.charAt(idx+1);
+    }
+
+    private final static String deleteDuplicateLetter(String checkStr, int idx){
+        return checkStr.substring(0,idx)+checkStr.substring(0,idx+2);
     }
 }
