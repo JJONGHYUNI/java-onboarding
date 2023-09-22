a41513d7df4c98c2ef458c09e4bc26111aca8b95
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-09-22 18:00:48 +0900

    [Feat] 숫자 범위 유효 검사 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,5 +5,12 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+    private final static boolean validateNumberRange(int number){
+        if(number<1 || number >1000){
+            return true;
+        }
+        return false;
+    }
 
+    private
 }
