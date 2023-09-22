0e062a04cf74a46782b4ed1ad1591d60a045ff09
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-09-22 18:16:38 +0900

    [Feat] 각 자릿수 숫자 List 구하는 함수 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,16 +1,29 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+    private final static int CONSTANT_TEN = 10;
     public static int solution(int number) {
         int answer = 0;
+        List<Integer> digitList = howDigit(number);
+        System.out.print(digitList.toString());
         return answer;
     }
     private final static boolean validateNumberRange(int number){
-        if(number<1 || number >1000){
+        if(number<1 || number >10000){
             return true;
         }
         return false;
     }
-
-    private
+    private final static List<Integer> howDigit(int number){
+        List<Integer> digitList = new ArrayList<>();
+        while(number/CONSTANT_TEN!=0){
+            digitList.add(number%CONSTANT_TEN);
+            number=number/CONSTANT_TEN;
+        }
+        digitList.add(number%CONSTANT_TEN);
+        return digitList;
+    }
 }
