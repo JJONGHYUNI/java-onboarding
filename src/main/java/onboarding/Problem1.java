53036227df4ae31075fbc3671ffba2943a00391e
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-09-22 15:18:37 +0900

    [Feat] Solution 내용 구현
    Signed-off-by: JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        char[] pobi1 = intToChar(pobi.get(0));
+        char[] pobi2 = intToChar(pobi.get(1));
+        char[] crong1 = intToChar(crong.get(0));
+        char[] crong2 = intToChar(crong.get(1));
+        int maxPobi = compare(compare(sum(pobi1),multi(pobi1)),compare(sum(pobi2),multi(pobi2)));
+        int maxCrong = compare(compare(sum(crong1),multi(crong1)),compare(sum(crong2),multi(crong2)));
         return answer;
+    }
+    private static int compare(int num1, int num2){
+        if(num1>num2){
+            return num1;
+        }
+        return num2;
     }
     private static int sum(char[] num){
         int Sum = 0;
