bb66a0f7c3fe083a456474aae56653131f10eb26
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-02 15:58:11 +0900

    [Feat] money의 범위가 유효한지 검사하는 함수 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private final static int MAX_LENGTH = 1000000;
+    private final static int MIN_LENGTH = 1;
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
     }
+
+    private static boolean validateMoneyRange(int money){
+        return money >= MIN_LENGTH && money <= MAX_LENGTH;
+    }
+    
 }
