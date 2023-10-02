0e9b35b249ec92bd479486d1d74351232f931a5d
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-02 16:15:49 +0900

    [Feat] 지폐의 개수와 남은 돈을 담을 class 구현
    Signed-off-by: JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -2,6 +2,7 @@ package onboarding;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class Problem5 {
     private final static int MAX_LENGTH = 1000000;
@@ -17,5 +18,18 @@ public class Problem5 {
     private static boolean validateMoneyRange(int money){
         return money >= MIN_LENGTH && money <= MAX_LENGTH;
     }
-    
+
+    private static DivisionMoney divisionForEachMoney(int currentMoney, int divisionMoney){
+        return new DivisionMoney(currentMoney/divisionMoney, currentMoney%divisionMoney);
+    }
+}
+
+class DivisionMoney {
+    int quotitent;
+    int remainder;
+
+    public DivisionMoney(int quotitent, int remainder){
+        this.quotitent = quotitent;
+        this.remainder = remainder;
+    }
 }
