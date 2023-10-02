a1be5a01f3e1f9d7ef744bb775cba92dc25aa01a
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-02 17:35:16 +0900

    [Feat] 데이터 유효성 검사 함수 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,16 +1,38 @@
 package onboarding;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
+    private final static String CHECK_DOMAIN = "email.com";
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<Member> memberList = forms.stream().map(form -> new Member(form.get(0),form.get(1))).collect(Collectors.toList());
+
         return answer;
     }
+    private static boolean validatecrewNumRange(int range){
+        return range>=1 && range<=10000;
+    }
+    private static boolean validateEamilRange(int range){
+        return range >= 11 && range < 20;
+    }
+    private static boolean validateEmail(String email){
+        int atIndex = email.indexOf('@');
+        String domain = email.substring(atIndex + 1);
+        return domain.equals(CHECK_DOMAIN);
+    }
 
+    private static boolean isNicknameKorean(String nickname){
+        return nickname.matches("^[가-힣]*$");
+    }
+
+    private static boolean validateNicknameRange(String nickname){
+        return nickname.length() >= 1 && nickname.length() < 20;
+    }
     private static class Member{
         private String email;
-        private String name;
+        private String nickname;
         private boolean duplicated;
 
         Member(String email, String name){
