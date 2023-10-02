33121565b5e65ea0064ad06016547ab15893d436
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-02 16:01:37 +0900

    [Feat] 지폐, 동전 종류 리스트 정적 상수 리스트로 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,6 +7,8 @@ public class Problem5 {
     private final static int MAX_LENGTH = 1000000;
     private final static int MIN_LENGTH = 1;
 
+    private final static int[] MONEY_LIST = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
     public static List<Integer> solution(int money) {
         List<Integer> answer = Collections.emptyList();
         return answer;
