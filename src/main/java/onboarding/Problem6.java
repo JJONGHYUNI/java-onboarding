5f465019e202cde90da35d52abe2ebd5b1fe53a8
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-10-03 18:21:19 +0900

    [Feat] customException 구현 및 제한 사항 적용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,6 +12,7 @@ public class Problem6 {
     private static HashSet<String> duplicateEmailList = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
         List<Member> memberList = forms.stream().map(form -> new Member(form.get(0),form.get(1))).collect(Collectors.toList());
+        checkValidation(memberList);
         memberList.forEach(member -> setTwoLetterList(member));
         saveDuplicateMember();
         List<String> answer = new ArrayList<>(duplicateEmailList)
@@ -20,7 +21,30 @@ public class Problem6 {
                 .collect(Collectors.toList());
         return answer;
     }
+    public static void checkValidation(List<Member> members) {
+        try {
+            if (!validatecrewNumRange(members.size())) {
+                throw new CustomException("크루는 1명 이상 10000명 이하입니다.");
+            }
 
+            for (Member member : members) {
+                if (!validateEmail(member.email)) {
+                    throw new CustomException("올바른 이메일이 아닙니다.");
+                }
+                if (!validateEamilRange(member.email.length())) {
+                    throw new CustomException("이메일의 범위는 11자 이상 20자 미만입니다.");
+                }
+                if (!validateNicknameRange(member.nickname)) {
+                    throw new CustomException("닉네임은 11자 이상 20자 미만입니다.");
+                }
+                if (!isNicknameKorean(member.nickname)) {
+                    throw new CustomException("닉네임은 한글만 가능합니다.");
+                }
+            }
+        }catch (CustomException e){
+            System.out.println(e.message);
+        }
+    }
     private static void saveDuplicateMember(){
         for(String key : twoLetterList.keySet()){
             List<Member> currentMembers = twoLetterList.get(key);
@@ -72,6 +96,13 @@ public class Problem6 {
         Member(String email, String name){
             this.email = email;
             this.nickname = name;
+        }
+    }
+
+    private static class CustomException extends RuntimeException{
+        String message;
+        public CustomException(String message){
+            this.message=message;
         }
     }
 }
